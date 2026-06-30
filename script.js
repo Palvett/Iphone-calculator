14eb7f20d3289f5f12ee6f3a5c1814e38329ad4b
Palvett Blaise <blaisepalvett@gmail.com>
2026-06-30 12:25:25 +0100

    linter corrections

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
 // functionality
 
-document.addEventListener("DOMContentLoaded", () => {
-  const display = document.querySelector(".display");
-  const buttons = document.querySelectorAll(".button");
-  const functionButtons = document.querySelectorAll(".function");
-  const operatorButtons = document.querySelectorAll(".operator");
-  const numberButtons = document.querySelectorAll(".number");
-  const decimalButton = document.querySelector(".decimal");
-  const equallButton = document.querySelector(".equal");
+document.addEventListener('DOMContentLoaded', () => {
+  const display = document.querySelector('.display')
+  const functionButtons = document.querySelectorAll('.function')
+  const operatorButtons = document.querySelectorAll('.operator')
+  const numberButtons = document.querySelectorAll('.number')
 
   numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (display.value === "0") {
-        display.value = button.textContent.trim();
+    button.addEventListener('click', () => {
+      if (display.value === '0') {
+        display.value = button.textContent.trim()
       } else {
-        display.value += button.textContent.trim();
+        display.value += button.textContent.trim()
       }
-    });
-  });
+    })
+  })
 
   operatorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const value = button.textContent.trim();
-      display.value += value;
-    });
-  });
+    button.addEventListener('click', () => {
+      const value = button.textContent.trim()
+      display.value += value
+    })
+  })
 
   functionButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const value = button.textContent.trim();
-
-      if (button.classList.contains("clear")) {
-        display.value = "0";
+    button.addEventListener('click', () => {
+      if (button.classList.contains('clear')) {
+        display.value = '0'
       }
 
-      if (button.classList.contains("back")) {
-        display.value = display.value.slice(0, -1) || "0";
+      if (button.classList.contains('back')) {
+        display.value = display.value.slice(0, -1) || '0'
       }
 
-      if (button.classList.contains("percentage")) {
-        display.value = (parseFloat(display.value) / 100).toString();
+      if (button.classList.contains('percentage')) {
+        display.value = (parseFloat(display.value) / 100).toString()
       }
 
-      if (button.classList.contains("toggle")) {
-        display.value = String(-parseFloat(display.value));
+      if (button.classList.contains('toggle')) {
+        display.value = String(-parseFloat(display.value))
       }
-    });
-  });
-});
+    })
+  })
+})
