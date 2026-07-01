c4428f58707d3080e104046f33cc22f85e2d66fe
Palvett Blaise <blaisepalvett@gmail.com>
2026-07-01 12:38:35 +0100

    linter corrections

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
     display.value = result
     adjustFontSize()
 
-    const item =document.createElement('li')
+    const item = document.createElement('li')
     item.textContent = `${expressionRaw} = ${result}`
     historyList.prepend(item)
   })
